9f2dfa1c624e961b89198599dd4441d7780e685c
Eduardc77 <36419167+eduardc77@users.noreply.github.com>
2025-06-02 23:42:54 +0300

    initial commit: add project files

---
+++ FloraList/FloraList/FloraListApp.swift
@@ -0,0 +1,17 @@
+//
+//  FloraListApp.swift
+//  FloraList
+//
+//  Created by User on 6/2/25.
+//
+
+import SwiftUI
+
+@main
+struct FloraListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FloraList/FloraListTests/FloraListTests.swift
@@ -0,0 +1,16 @@
+//
+//  FloraListTests.swift
+//  FloraListTests
+//
+//  Created by User on 6/2/25.
+//
+
+import Testing
+
+struct FloraListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
